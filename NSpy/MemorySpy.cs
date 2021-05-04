1ab3b74afbe1f9caf4857d845f205814e97bad19
zmjack <jack@zmland.com>
2021-05-04 16:26:00 +0800

    v0.1.0

--- NSpy/MemorySpy.cs
+++ NSpy/MemorySpy.cs
@@ -1,7 +1,9 @@
 ﻿using NStandard;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace NSpy
 {
@@ -10,31 +12,65 @@ namespace NSpy
         public static unsafe byte[] GetBytes<T>(T instance) where T : unmanaged => GetBytes(&instance, Marshal.SizeOf(instance));
         public static unsafe byte[] GetBytes(void* pointer, int size) => new byte[size].Let(i => *((byte*)pointer + i));
 
-        public static unsafe string GetHexString<T>(T instance) where T : unmanaged => GetHexStrings(instance, 0)[0];
-        public static unsafe string GetHexString(void* pointer, int size) => GetHexStrings(pointer, size, 0)[0];
-        public static unsafe string GetHexString(byte[] bytes) => GetHexStrings(bytes, 0)[0];
-        public static unsafe string[] GetHexStrings<T>(T instance, int columns) where T : unmanaged => GetHexStrings(&instance, Marshal.SizeOf(instance), columns);
-        public static unsafe string[] GetHexStrings(void* pointer, int size, int columns) => GetHexStrings(GetBytes(pointer, size), columns);
-        public static unsafe string[] GetHexStrings(byte[] bytes, int columns)
+        #region Memory
+        public static unsafe string GetHexString<T>(T instance) where T : unmanaged => GetString(ConvertToHexString, GetBytes(instance));
+        public static unsafe string GetHexString(void* pointer, int size) => GetString(ConvertToHexString, GetBytes(pointer, size));
+        public static unsafe string GetHexString(byte[] bytes) => GetString(ConvertToHexString, bytes);
+        public static unsafe string GetHexString<T>(T instance, int columns) where T : unmanaged => GetString(ConvertToHexString, GetBytes(instance), columns);
+        public static unsafe string GetHexString(void* pointer, int size, int columns) => GetString(ConvertToHexString, GetBytes(pointer, size), columns);
+        public static unsafe string GetHexString(byte[] bytes, int columns) => GetString(ConvertToHexString, bytes, columns);
+
+        public static unsafe string GetBinaryString<T>(T instance) where T : unmanaged => GetString(ConvertToBinaryString, GetBytes(instance));
+        public static unsafe string GetBinaryString(void* pointer, int size) => GetString(ConvertToBinaryString, GetBytes(pointer, size));
+        public static unsafe string GetBinaryString(byte[] bytes) => GetString(ConvertToBinaryString, bytes);
+        public static unsafe string GetBinaryString<T>(T instance, int columns) where T : unmanaged => GetString(ConvertToBinaryString, GetBytes(instance), columns);
+        public static unsafe string GetBinaryString(void* pointer, int size, int columns) => GetString(ConvertToBinaryString, GetBytes(pointer, size), columns);
+        public static unsafe string GetBinaryString(byte[] bytes, int columns) => GetString(ConvertToBinaryString, bytes, columns);
+        #endregion
+
+        #region Sequence
+        public static unsafe string GetSequenceHexString<T>(T instance) where T : unmanaged => GetSequenceString(ConvertToHexString, GetBytes(instance));
+        public static unsafe string GetSequenceHexString(void* pointer, int size) => GetSequenceString(ConvertToHexString, GetBytes(pointer, size));
+        public static unsafe string GetSequenceHexString(byte[] bytes) => GetSequenceString(ConvertToHexString, bytes);
+        public static unsafe string GetSequenceHexString<T>(T instance, int columns) where T : unmanaged => GetSequenceString(ConvertToHexString, GetBytes(instance), columns);
+        public static unsafe string GetSequenceHexString(void* pointer, int size, int columns) => GetSequenceString(ConvertToHexString, GetBytes(pointer, size), columns);
+        public static unsafe string GetSequenceHexString(byte[] bytes, int columns) => GetSequenceString(ConvertToHexString, bytes, columns);
+
+        public static unsafe string GetSequenceBinaryString<T>(T instance) where T : unmanaged => GetSequenceString(ConvertToBinaryString, GetBytes(instance));
+        public static unsafe string GetSequenceBinaryString(void* pointer, int size) => GetSequenceString(ConvertToBinaryString, GetBytes(pointer, size));
+        public static unsafe string GetSequenceBinaryString(byte[] bytes) => GetSequenceString(ConvertToBinaryString, bytes);
+        public static unsafe string GetSequenceBinaryString<T>(T instance, int columns) where T : unmanaged => GetSequenceString(ConvertToBinaryString, GetBytes(instance), columns);
+        public static unsafe string GetSequenceBinaryString(void* pointer, int size, int columns) => GetSequenceString(ConvertToBinaryString, GetBytes(pointer, size), columns);
+        public static unsafe string GetSequenceBinaryString(byte[] bytes, int columns) => GetSequenceString(ConvertToBinaryString, bytes, columns);
+        #endregion
+
+        private static string ConvertToHexString(byte x) => x.ToString("x2").PadLeft(2, '0');
+        private static string ConvertToBinaryString(byte x) => Convert.ToString(x, 2).PadLeft(8, '0');
+
+        private static string GetString(Func<byte, string> convert, IEnumerable<byte> bytes)
         {
-            var ret = bytes.Select(x => x.ToString("x2").PadLeft(2, '0')).Join(" ");
-            if (columns > 0)
-                return StringEx.SplitIntoLines(ret, 3 * columns);
-            else return new[] { ret };
+            if (!bytes.Any()) return string.Empty;
+
+            var sb = new StringBuilder();
+            sb.AppendLine(bytes.Select(x => convert(x)).Join(" "));
+            sb.Length -= Environment.NewLine.Length;
+            return sb.ToString();
         }
 
-        public static unsafe string GetBinaryString<T>(T instance) where T : unmanaged => GetBinaryStrings(instance, 0)[0];
-        public static unsafe string GetBinaryString(void* pointer, int size) => GetBinaryStrings(pointer, size, 0)[0];
-        public static unsafe string GetBinaryString(byte[] bytes) => GetBinaryStrings(bytes, 0)[0];
-        public static unsafe string[] GetBinaryStrings<T>(T instance, int columns = 0) where T : unmanaged => GetBinaryStrings(&instance, Marshal.SizeOf(instance), columns);
-        public static unsafe string[] GetBinaryStrings(void* pointer, int size, int columns = 0) => GetBinaryStrings(GetBytes(pointer, size), columns);
-        public static unsafe string[] GetBinaryStrings(byte[] bytes, int columns = 0)
+        private static string GetString(Func<byte, string> convert, IEnumerable<byte> bytes, int columns)
         {
-            var ret = bytes.Select(x => Convert.ToString(x, 2).PadLeft(8, '0')).Join(" ");
-            if (columns > 0)
-                return StringEx.SplitIntoLines(ret, 9 * columns);
-            else return new[] { ret };
+            if (!bytes.Any()) return string.Empty;
+            if (columns <= 0) throw new ArgumentException($"The {nameof(columns)} must be greater than 0.");
+
+            var sb = new StringBuilder();
+            var groups = bytes.Select((v, i) => new { Key = i, Value = v }).GroupBy(x => x.Key / columns, x => x.Value);
+            foreach (var group in groups) sb.AppendLine(group.Select(x => convert(x)).Join(" "));
+            sb.Length -= Environment.NewLine.Length;
+            return sb.ToString();
         }
+
+        private static string GetSequenceString(Func<byte, string> convert, byte[] bytes) => GetString(convert, bytes.Reverse());
+        private static string GetSequenceString(Func<byte, string> convert, byte[] bytes, int columns) => GetString(convert, bytes.Reverse(), columns);
 
     }
 }
